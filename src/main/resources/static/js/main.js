5ef37278623416766ea1c64370aba5502d036cd3
weidongyue <912043147@qq.com>
2018-10-28 21:19:22 +0800

    增加页面

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,26 +2,42 @@
  * Created by Administrator on 2018/10/28.
  */
 
-function showAddBox(){
-    var addBox = document.getElementById("addBox");
-    addBox.style.display = "block";
-}
+//function showAddBox(){
+//    var addBox = document.getElementById("addBox");
+//    addBox.style.display = "block";
+//}
 function data1(){
     var date = new Date();
     var year = date.getFullYear();
-    var mongth = date.getMonth()+1;
+    var month = date.getMonth()+1;
     var day = date.getDate();
-    var time1 = document.getElementById("time1");
-    time1.value = year + "-" + mongth + "-" + day;
+    var creatDate = document.getElementById("creatDate");
+    creatDate.value = year + "-" + month + "-" + day;
 }
 
-    function hideDiv(){
-        $("#addBox").css("display","none");
-        $("#bg2").css("display","none");
-    }
+function hideDiv(){
+     $("#addBox").hide();
+     $("#bg2").hide();
+}
 function show(){
     $(function(){
         $("#bg2").show().fadeTo(2000,.9);
         $("#addBox").delay(1000).show();
     });
 }
+function del(){
+    $("#name").val("");
+    $("#age").val("");
+    $("#sex").val("");
+    $("#classNumber").val("");
+    $("#teacher").val("");
+    $("#EnrollmentYear").val("");
+}
+function send1(){
+    alert("添加成功了");
+}
+function modify(){
+    alert("修改成功");
+}
+
+
