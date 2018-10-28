1b9d5d738afabd6740ec198be906280a3c2976a7
liuweibang <913635030@qq.com>
2018-10-28 19:00:55 +0800

    删除学生

--- src/main/java/com/suixingpay/sms/student/controller/StudentsController.java
+++ src/main/java/com/suixingpay/sms/student/controller/StudentsController.java
@@ -21,51 +21,35 @@ public class StudentsController {
 	@Autowired
 	private StudentService studentService;
 
-	@GetMapping("/saveStudent")
-	@ResponseBody
-	public Map<String,Object> saveStudent(){
-       //@RequestParam(value="callback",required=false) String callback
-		//JSONObject obj = new JSONObject();
-	//	obj.put("name", "li");
-	//	MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(obj);
-	//	if(callback!=null){
-	//		mappingJacksonValue.setJsonpFunction("callbackname");
-	//	}
-	//	return mappingJacksonValue;
-
-		//@RequestBody ResultData data
-		Student student = new Student();
-		student.setAge(20);
-		Student student2 = new Student();
-		student2.setStatus("keyong");
-
-		List<Student> students = new ArrayList<>();
-		students.add(student);
-		students.add(student2);
-		//System.out.println(data.getKey());
-		//System.out.println(data.getStudentList());
-		//@RequestBody Student student
-		System.out.println("ssd");
-		System.out.println("进来了");
-		Map<String,Object> map = new HashMap<>();
-		map.put("result",students);
-		//studentService.saveStudent(student)
-		//map.put("msg","测试成功");
-		//mainMap.put("json头",map);
-       return map;
-
-	}
-
 	/**
 	 * @author 刘维邦
 	 * @param student
 	 */
 	@GetMapping("/deleteStudent")
-	@ResponseBody
-	public void deleteStudent(Student student){
-		JDBC G = JDBC.getInstance();
-		G.delete(student);
-	}
+    @ResponseBody
+    public void deleteStudent(Student student){
+        JDBC G = JDBC.getInstance();
+        G.delete(student);
+    }
+    @GetMapping("/insertStudent")
+    @ResponseBody
+    public void insertStudent(Student student){
+        JDBC G = JDBC.getInstance();
+        G.insert(student);
+    }
+    @GetMapping("/selectStudent")
+    @ResponseBody
+    public void selectStudent(String... names){
+        JDBC G = JDBC.getInstance();
+        G.selectList(names);
+    }
+    @GetMapping("/updateStudent")
+    @ResponseBody
+    public void updateStudent(Student student){
+        JDBC G = JDBC.getInstance();
+        G.update(student);
+    }
+
 	@GetMapping("/test")
 	public String text(){
 		return "index";
